ca71e4e0501adfec0b3551864d0df7d1c7bcd739
Joana-CSilva29 <144342465+Joana-CSilva29@users.noreply.github.com>
2024-01-26 10:28:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from classes.simulation import Simulation
 
 Corridor = Lattice(40, 100)
 
-simulation = Simulation(300, 1000, Corridor)
+simulation = Simulation(300, 100, Corridor)
 print(Corridor)
 # simulation.plot_snapshot()
 
