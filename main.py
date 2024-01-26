f9bcbb54b690416c6fd99a27675972456fba551e
Nina van der Meulen <ninaajwzz@gmail.com>
2024-01-26 15:30:03 +0100

    small changes

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ from helpers import build_and_save_animation
 
 x_width = 50
 y_height = 50
-density = 0.002
+density = 0.1
 N = int(density*x_width*y_height)
-iterations = 20
+iterations = 350
 Corridor = Lattice(x_width, y_height)
 
 simulation = Simulation(N, iterations, Corridor)
 print(Corridor)
 
-data_frames, sigma_values = simulation.run(animate=True)
+data_frames = simulation.run(animate=True)
 
 build_and_save_animation(data_frames, f'test_rho_{density}', iterations)
 
 # plotting the order parameter
-plt.figure()
-plt.plot(np.linspace(1, iterations, iterations), sigma_values)
-plt.show()
+# plt.figure()
+# plt.plot(np.linspace(1, iterations, iterations), sigma_values)
+# plt.show()
