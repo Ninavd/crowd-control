ae62cc4feccd605b6639324dd2ad45446f9a83c8
Nina van der Meulen <ninaajwzz@gmail.com>
2024-01-29 00:57:38 +0100

    running via the CLI implemented

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import scipy.stats as stats
 import pandas as pd
@@ -6,38 +7,69 @@ from classes.lattice import Lattice
 from classes.simulation import Simulation
 from helpers import build_and_save_animation
 
-x_width = 50
-y_height = 50
-density = 0.12
-N = int(density * x_width * y_height)
-iterations = 500
 
-Corridor = Lattice(x_width, y_height)
-Corridor.populate_corridor(N)
+def main(density, iterations, runs, L, animate, save_video, save_results):
+    
+    N = int(density * L * L)
+    
+    corridor = Lattice(L, L)
+    corridor.populate_corridor(N)
 
-simulation = Simulation(iterations, Corridor)
+    for i in range(runs):
+        simulation = Simulation(iterations, corridor)
+        images, phi_values = simulation.run(animate=animate, save_video=save_video)
 
-data_frames, phi_values = simulation.run(animate=False)
-
-build_and_save_animation(data_frames, f'test_rho_{density}', iterations)
-
-
-def datacsv(it,output_file):
-    results = []
-    all_phi = []
-    for _ in range(it):
-        simulation = Simulation(iterations, Corridor)
-        _, phi_values = simulation.run(animate=True)
-        all_phi.append(phi_values)
-
-    mean_phi = np.mean(all_phi, axis = 0)
-    variance_phi = np.var(all_phi, axis = 0, ddof=1)
-    df_results = pd.DataFrame({'mean_phi': mean_phi, 'variance': variance_phi})
-    df_results.to_csv(output_file)
-    print(f"Results written to {output_file}")
-
-it_analyze = 2
-output_file = "simulation_results.csv"
-datacsv(it_analyze,output_file)
+        if save_video:
+            build_and_save_animation(images, f'rho_{density}', iterations)
         
+        if save_results:
+            simulation.plot_results(phi_values, save=True)
+            df = pd.DataFrame({'phi':phi_values})
+            output_file = f'./results/run_{i}_rho_{density}_L_{L}_iters_{iterations}.csv'
+            df.to_csv(output_file)
+            print(f"Results written to {output_file}")
+
+    # def datacsv(it,output_file):
+    #     results = []
+    #     all_phi = []
+    #     for _ in range(it):
+    #         simulation = Simulation(iterations, corridor)
+    #         _, phi_values = simulation.run(animate=True)
+    #         all_phi.append(phi_values)
+
+    #     mean_phi = np.mean(all_phi, axis = 0)
+    #     variance_phi = np.var(all_phi, axis = 0, ddof=1)
+    #     df_results = pd.DataFrame({'mean_phi': mean_phi, 'variance': variance_phi})
+    #     df_results.to_csv(output_file)
+    #     print(f"Results written to {output_file}")
+
+    # it_analyze = 2
+    # output_file = "simulation_results.csv"
+    # datacsv(it_analyze,output_file)
         
+if __name__ == '__main__':
+
+    # set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description="Simulate lane formation in heterogenic crowds")
+
+    # adding arguments
+    parser.add_argument("density", help="density of the crowd on the lattice", default=0.1, type=float)
+    parser.add_argument("iterations", help="number of timesteps executed per run", default=250, type=int)
+    parser.add_argument("-n", "--runs", help="number of runs", default=1, type=int)
+    parser.add_argument("-s", "--size", help="size of the L x L lattice", default=50, type=int)
+
+    parser.add_argument("-v", "--animate", action="store_true", help="visualize the simulation while running")
+    parser.add_argument("--save_video", action="store_true", help="save simulation visuals to mp4")
+    parser.add_argument("--save_results", action="store_true", help="store all data in a csv for each simulation/run")
+
+    # read arguments from command line
+    args = parser.parse_args()
+
+    # print error if arguments invalid, else run main with provided arguments
+    if args.density > 1 or args.density < 0:
+        print("Density must lie between 0 and 1.")
+    else:
+        main(
+            args.density, args.iterations, args.runs, args.size, 
+            args.animate, args.save_video, args.save_results
+            )   
