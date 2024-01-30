90714208184453b49017a28f23da7aedd2707b24
Nina van der Meulen <ninaajwzz@gmail.com>
2024-01-30 12:42:26 +0100

    added for easier running

---
+++ bash_scripts/runs_part_7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rho_values=(0.26 0.28)
+p_values = (0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+
+for rho in "${rho_values[@]}"
+do
+    for p in "${p_values[@]}"
+    do
+        python3 main.py $rho 2000 -n 10 -p $p --save_results
+    done
+done
