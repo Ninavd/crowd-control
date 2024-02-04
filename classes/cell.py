01db4de32730daa5453cda3ea60dea7b90c50037
Nina van der Meulen <ninaajwzz@gmail.com>
2024-02-04 18:12:00 +0100

    cleanup + docstrings updated

--- classes/cell.py
+++ classes/cell.py
@@ -5,8 +5,31 @@ from numpy import ndarray
 from helpers import get_distance_array, is_empty_vectorized
 
 class Cell:
+    """
+    A cell represents a square on a lattice. 
+    It may be empty or populated and has a fixed position in the lattice.
+
+    Attributes:
+        value (int): Defines if cell is populated (+/- 1) or empty (0).
+        x (int): Row number of the cell
+        y (int): Column number of the cell
+        left_exit_distance (float):  Distance to leftt side of corridor
+        right_exit_distance (float): Distance to right side of corridor
+        neighbors (list[Cell]): List of neighboring cells in the lattice. 
+    """
 
     def __init__(self, x, y, left_exit_distance, right_exit_distance) -> None:
+        """
+        Initializes empty Cell.
+
+        Args:
+            value (int): Defines if cell is populated (+/- 1) or empty (0).
+            x (int): Row number of the cell.
+            y (int): Column number of the cell.
+            left_exit_distance (float):  Distance to leftt side of corridor.
+            right_exit_distance (float): Distance to right side of corridor.
+        """
+        
         self.value = 0
         self.left_exit_distance = left_exit_distance
         self.right_exit_distance = right_exit_distance
@@ -35,6 +58,9 @@ class Cell:
     def populate(self, value) -> None:
         """
         Populate a cell if it is not empty.
+
+        Args:
+            value (int): The movement direction (-1 or +1)
         """
         assert value in [-1, 1], 'value must be 1 or -1'
         assert self.value == 0, 'can not populate un-empty cell'
@@ -50,6 +76,9 @@ class Cell:
     def add_neighbor(self, cell) -> None:
         """
         Add neighbouring cell object.
+
+        Args:
+            cell (object): Neighboring cell to be added.
         """
         assert len(self.neighbors) < 9, 'max number of neighbors exceeded.'
 
@@ -58,7 +87,11 @@ class Cell:
     def get_distance_value(self, value) -> int:
         """
         Get the correct distance value based on who is asking.
+
+        Args:
+            value (int): The movement direction (-1 or +1)
         """
+        assert value != 0, 'value is ambiguous, must be -1 or 1'
         return self.right_exit_distance if value > 0 else self.left_exit_distance
     
     def get_empty_neighbors(self) -> ndarray[object]:
@@ -70,9 +103,14 @@ class Cell:
     
     def get_best_neighbor(self, p) -> object | None:
         """
-        Find neighbor cell with smallest distance to the relevant exit.
-        Only looks at empty cells for now. 
+        Return empty neighbor cell with smallest distance to the exit.
+        Return None if no empty options found.
+
+        Args:
+            p (float): Probability of choosing a horizontal neighbor
         """
+        assert p >= 0 and p <= 1, 'p is a probability'
+
         # only consider empty neighbors
         empty_neighbors = self.get_empty_neighbors()
 
@@ -88,12 +126,6 @@ class Cell:
         if distances.min() >= current_distance:
             return None 
         
-        # TODO: choose smallest distance first ofzo
-        # # min_dist_neighbors = empty_neighbors[distances == distances.min()]
-        # min_distance_count = sum(distances[distances == distances.min()])
-        # if min_distance_count == 1 and random.random() < 0.8:
-        #     return empty_neighbors[distances == distances.min()][0]
-  
         # only consider neighbors with smaller distance to exit
         empty_neighbors = empty_neighbors[distances < current_distance]
 
@@ -104,24 +136,20 @@ class Cell:
         for i, neighbor in enumerate(empty_neighbors):
 
             if neighbor.x == self.x:
-                
                 if random.random() < p:
                     return neighbor
-                
                 np.delete(empty_neighbors, i)
         
-        # otherwise target a diagonal cell
+        # otherwise target one of remaining cells
         return np.random.choice(empty_neighbors)
     
     def lower_distance_to_exit(self):
-        '''
-        This function lowers the value of the distance to the exit
-        In an effort to create a dynamic floorplan in which
-        people prefer behind someone else because the probability
-        of collisions is lower.
-        '''
+        """
+        Dynamically lower the distance to the exit.
+        Only call this method when agent is leaving the cell. 
+        """
         if self.value < 0:
-            self.left_exit_distance += -0.00001
+            self.left_exit_distance -= 0.00001
 
         elif self.value > 0:
-            self.right_exit_distance += -0.00001
+            self.right_exit_distance -= 0.00001
